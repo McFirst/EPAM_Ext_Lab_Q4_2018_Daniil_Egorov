42f34ae7688d73299b2ad89d6187d75b22e37371
Daniil Egorov <mcfirst87@gmail.com>
2018-11-30 22:59:20 +0400

    начало работы

---
+++ Task 3/Task 3/Class31.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3
+{
+    class Class31
+    {
+        public decimal a;
+        public decimal b;
+
+        public decimal Square()
+        {
+            decimal x = a * b;
+            return x;
+        }
+    }
+}
---
+++ Task 3/Task 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //примеры https://metanit.com/sharp/tutorial/2.8.php
+
+
+            Console.ReadKey();
+        }
+    }
+}
