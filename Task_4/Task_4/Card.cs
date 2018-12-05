4a9134b2aee96fb8d13b7f328a75f380176a7519
Daniil Egorov <mcfirst87@gmail.com>
2018-12-05 22:41:31 +0400

    Начало работы

---
+++ Task_4/Task_4/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task_4
+{
+    public class Card : Material
+    {
+        public int Rating
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public int Voicecount
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public void Find()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Task_4/Task_4/Material.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task_4
+{
+    public class Material
+    {
+        public string Autor
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+
+        public string Type
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+
+        public string Content
+        {
+            get => default(string);
+            set
+            {
+            }
+        }
+
+        public DateTime Datecreate
+        {
+            get => default(DateTime);
+            set
+            {
+            }
+        }
+    }
+}
