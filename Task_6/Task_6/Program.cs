5ba793398b4090011c6e99d64e0e4bc096a6889f
Daniil Egorov <mcfirst87@gmail.com>
2018-12-24 21:57:17 +0400

    начало выполнения. 6.1 сделано

---
+++ Task_6/Task_6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = new string[] { "abcd", "pcd", "abcde", "bbc", "a" };
+            SortLength sl = new SortLength();
+            sl.RegisterComp(onStrcompare);
+
+            foreach (string i in lines)
+                Console.WriteLine(i);
+            Console.WriteLine();
+
+            foreach (string i in sl.StringSort(lines))
+                Console.WriteLine(i);
+
+            //Console.WriteLine(String.Compare("abc", "pbc"));
+            Console.ReadLine();
+        }
+
+        public static bool onStrcompare(string str1, string str2)
+        {
+            bool ret = false;
+            if (str1.Length > str2.Length)
+                ret = true;
+            else if (str1.Length == str2.Length)
+            {
+                if (String.Compare(str1, str2) > 0)
+                    ret = true;
+            }
+            return ret;
+        }
+    }
+}
---
+++ Task_6/Task_6/SortLength.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    class SortLength
+    {
+        #region DelegateBlock
+        //////////////////////////////////////////////////////////////////////////////////
+        
+        /// <summary>
+        /// Тип делегата
+        /// </summary>
+        /// <param name="str1"></param>
+        /// <param name="str2"></param>
+        public delegate bool Strcompare(string str1, string str2);
+
+        /// <summary>
+        /// Переcwменная типа делегата
+        /// </summary>
+        private Strcompare listcomp;
+
+        /// <summary>
+        /// Метод для регистрации обработного вызова (для инкапсуляции)
+        /// </summary>
+        /// <param name="methodToCall"></param>
+        public void RegisterComp(Strcompare methodToCall)
+        {
+            listcomp = methodToCall;
+        }
+
+        /// <summary>
+        /// Метод для отвязки
+        /// </summary>
+        /// <param name="methodToCall"></param>
+        public void UnRegisteComp(Strcompare methodToCall)
+        {
+            listcomp -= methodToCall;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+        #endregion
+
+        private string[] lines;
+
+        public string[] StringSort(string[] input)
+        {
+            lines = input;
+            string str = null;
+            for (int i = 0; i < lines.Length - 1; i++)
+            {
+                for (int j = i + 1; j < lines.Length; j++)
+                {
+                    if (listcomp(lines[i], lines[j]))
+                    {
+                        str = lines[i];
+                        lines[i] = lines[j];
+                        lines[j] = str;
+                    }
+                }
+            }
+            return lines;
+        }
+    }
+}
