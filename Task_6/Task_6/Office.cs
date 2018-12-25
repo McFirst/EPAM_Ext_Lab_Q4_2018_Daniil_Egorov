99b2ed76d34192b5d7cc26b4d9b634b664abaa34
McFirst <45260930+McFirst@users.noreply.github.com>
2018-12-25 17:17:47 +0400

    почему-то не загрузилось

---
+++ Task_6/Task_6/Office.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    class Office
+    {
+        #region DelegateBlock
+        //////////////////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// тип делегат прихода
+        /// </summary>
+        /// <param name="user">сотрудник</param>
+        /// <param name="time">время прихода</param>
+        public delegate void Comein(Person user, int time);
+
+        /// <summary>
+        /// тип делегат ухода
+        /// </summary>
+        /// <param name="user"></param>
+        public delegate void Comeout(Person user);
+
+        /// <summary>
+        /// переменная делегата прихода
+        /// </summary>
+        public event  Comein Income;
+
+        /// <summary>
+        /// переменная делегата прихода
+        /// </summary>
+        public event Comeout Outcom;
+
+        //////////////////////////////////////////////////////////////////////////////////
+        #endregion
+
+        ///
+    }
+}
