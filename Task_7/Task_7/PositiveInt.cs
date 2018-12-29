a810eac0eba7a46e57646e74bcb0ac944405ef21
Daniil Egorov <mcfirst87@gmail.com>
2018-12-29 11:21:46 +0400

    Вроде готово 7

--- Task_7/Task_7/PositiveInt.cs
+++ Task_7/Task_7/PositiveInt.cs
@@ -31,37 +31,5 @@ namespace Task_7
             }
             return result;
         }
-
-        /// <summary>
-        /// Является ли строка положительным числом, иначе False
-        /// </summary>
-        /// <param name="param"></param>
-        /// <returns></returns>
-        public static bool IsPositive(this string param)
-        {
-            /// System.FormatException
-            bool result = false;
-            try
-            {
-                if (param.GetType() == typeof(int))
-                {
-                    if (Convert.ToInt32(param) > 0) { result = true; }
-                }
-                if (param.GetType() == typeof(double))
-                {
-                    if (Convert.ToDouble(param) > 0) { result = true; }
-                }
-                if (param.GetType() == typeof(decimal))
-                {
-                    if (Convert.ToDecimal(param) > 0) { result = true; }
-                }
-
-            }
-            catch (System.FormatException e)
-            {
-                /// лог ошибки
-            }
-            return result;
-        }
     }
 }
