dc23c18d5b6d339224d65b430f3d28f6a7a00091
Daniil Egorov <mcfirst87@gmail.com>
2019-02-04 23:07:05 +0400

    6.3

--- Task_8/Query.sql
+++ Task_8/Query.sql
@@ -2,7 +2,7 @@ use [Northwind]
 --TASK_8.1
 ----1.1
 /*SELECT [OrderID], [ShippedDate], [ShipVia]
-FROM [Northwind].[dbo].[Orders]
+FROM [dbo].[Orders]
 WHERE [ShippedDate] >= CONVERT(DATETIME, '1998-05-06', 120) AND [ShipVia] >= 2
 */
 ----1.2
@@ -11,7 +11,7 @@ CASE
  WHEN [ShippedDate] IS NULL 
  THEN 'Not Shipped'
  END [ShippedDate]
-FROM [Northwind].[dbo].[Orders]
+FROM [dbo].[Orders]
 WHERE [ShippedDate] is NULL
 */
 ----1.3
@@ -20,26 +20,26 @@ CASE
  WHEN [ShippedDate] IS NULL 
  THEN 'Not Shipped'
  END [ShippedDate]
-FROM [Northwind].[dbo].[Orders]
+FROM [dbo].[Orders]
 WHERE [ShippedDate] > CONVERT(DATETIME, '1998-05-06', 120) OR [ShippedDate] is NULL
 */
 
 --TASK_8.2
 ----2.1
 /*SELECT [ContactName], [Country]
-FROM [Northwind].[dbo].[Customers]
+FROM [dbo].[Customers]
 WHERE [Country] IN ('USA','Canada')
 ORDER BY [ContactName], [Country]
 */
 ----2.2
 /*SELECT [ContactName], [Country]
-FROM [Northwind].[dbo].[Customers]
+FROM [dbo].[Customers]
 WHERE [Country] IN ('USA','Canada')
 ORDER BY [ContactName]
 */
 ----2.3
 /*SELECT DISTINCT [Country]
-FROM [Northwind].[dbo].[Customers]
+FROM [dbo].[Customers]
 WHERE [Country] IS NOT NULL
 ORDER BY [Country]
 */
@@ -47,18 +47,18 @@ ORDER BY [Country]
 --TASK_8.3
 ----3.1
 /*SELECT DISTINCT [OrderID]
-FROM [Northwind].[dbo].[Order Details]
+FROM [dbo].[Order Details]
 WHERE [Quantity] BETWEEN 3 AND 10
 */
 ----3.2
 /*SELECT [CustomerID], [Country]
-FROM [Northwind].[dbo].[Customers]
+FROM [dbo].[Customers]
 WHERE [Country] BETWEEN 'B*' AND 'G*'
 ORDER BY [Country]
 */
 ----3.3
 /*SELECT [CustomerID], [Country]
-FROM [Northwind].[dbo].[Customers]
+FROM [dbo].[Customers]
 WHERE [Country] >= 'B*' AND [Country] <= 'G*'
 ORDER BY [Country]
 */
@@ -66,42 +66,54 @@ ORDER BY [Country]
 --TASK_8.4
 ----4.1
 /*SELECT [ProductName]
-FROM [Northwind].[dbo].[Products]
+FROM [dbo].[Products]
 WHERE [ProductName] like '%cho_olade%'
 */
 
 --TASK_8.5
 ----5.1
 /*SELECT CONVERT(money,ROUND(sum(([UnitPrice]-[UnitPrice]*[Discount])*[Quantity]),2),1) AS [Totals]
-FROM [Northwind].[dbo].[Order Details] 
+FROM [dbo].[Order Details] 
 */
 ----5.2
 /*SELECT COUNT(*) - COUNT([ShippedDate]) AS [Count Orders]
-FROM [Northwind].[dbo].[Orders]
+FROM [dbo].[Orders]
 */
 ----5.3
 /*SELECT count(DISTINCT [CustomerID]) AS [Count CustomerID]
-FROM [Northwind].[dbo].[Orders]
+FROM [dbo].[Orders]
 */
 
 --TASK_8.6
 ----6.1
 /*SELECT Year([OrderDate]) AS [Year], COUNT([OrderID]) AS [Total]
-FROM [Northwind].[dbo].[Orders]
+FROM [dbo].[Orders]
 GROUP BY Year([OrderDate])
 ORDER BY [Year]
 
 SELECT COUNT(*) as [Total]
-FROM [Northwind].[dbo].[Orders]
+FROM [dbo].[Orders]
 */
 ----6.2
-SELECT (
-	select [LastName]+' '+[FirstName] from [dbo].[Employees]
-	where [Employees].[EmployeeID] = [Orders].[EmployeeID]
+/*SELECT (
+	SELECT [LastName]+' '+[FirstName] FROM [dbo].[Employees]
+	WHERE [Employees].[EmployeeID] = [Orders].[EmployeeID]
 		) AS [Seller]
 		, COUNT([OrderID]) AS [Total]
 FROM [dbo].[Orders]
 GROUP BY [EmployeeID]
 ORDER BY [Total]
-
+*/
 ----6.3
+SELECT
+	(SELECT [LastName]+' '+[FirstName] FROM [dbo].[Employees]
+	WHERE [Employees].[EmployeeID] = [Orders].[EmployeeID]
+		) AS [Seller] 
+	,(SELECT [ContactName] FROM [dbo].[Customers]
+	WHERE [Customers].[CustomerID] = [Orders].[CustomerID]
+		) AS [Customer]
+	,COUNT([OrderID]) AS [Amount]
+FROM [dbo].[Orders]
+WHERE Year([OrderDate]) = 1998
+GROUP BY CUBE ([EmployeeID], [CustomerID])
+ORDER BY [Amount] DESC
